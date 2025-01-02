adc3fc49489351a3dd0bd825228b9d665745a19f
Cameron Stephen <cameron@codeheroes.co.uk>
2025-01-02 14:48:30 +0000

    Update file structure to match PSR-4 standard

--- svg-inline-cache-manager.php
+++ svg-inline-cache-manager.php
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-require_once plugin_dir_path(__FILE__) . 'includes/class-svg-inline-cache-manager.php';
+require_once plugin_dir_path(__FILE__) . 'includes/SVGInlineCacheManager.php';
 require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
 
 new SVGInlineCacheManager();
